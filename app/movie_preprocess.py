704837fe34146937b58b0c23732f804960d07916
108-0 <jjang030819@gmail.com>
2025-03-24 12:19:03 +0900

    2025_03_24 오전수업 마무리

--- app/movie_preprocess.py
+++ app/movie_preprocess.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 movies_df = pd.read_csv("data/movies.csv")
 print(movies_df)
 
@@ -7,4 +8,12 @@ movies_df['movieId'] = movies_df['movieId'].astype(str)
 links_df = pd.read_csv('data/links.csv', dtype=str)
 merged_df = movies_df.merge(links_df, on='movieId', how='left')
 print(merged_df)
-print(movies_df.columns)
+print(merged_df.columns)
+
+def add_url(row):
+    return f"https://www.imdb.com/title/tt{row}"
+
+merged_df['url'] = merged_df['imdbId'].apply(lambda x:add_url(x))
+
+print(merged_df)
+print(merged_df.columns)
